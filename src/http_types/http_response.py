726cd2589774c998a20a1622fb921f58a8ee9b65
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2024-04-02 23:52:16 -0300

    fix: Arrumando erro de digitacao da classe response

--- src/http_types/http_response.py
+++ src/http_types/http_response.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-class HttpReponse:
+class HttpResponse:
   def __init__(self,body: Dict,status_code: int) -> None:
     self.body = body
     self.status_code = status_code
